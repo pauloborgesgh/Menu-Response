3b02d49b1574490ec9a51991376e0e78730000e6
pauloborgesgh <pauloborgeszambon@gmail.com>
2024-05-27 21:02:42 -0300

    deploy-in-site

---
+++ Script/Script.js
@@ -0,0 +1,12 @@
+
+function MostrarSideBar(){
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+
+    };
+
+    function EsconderSideBar(){
+    let sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+
+    }
